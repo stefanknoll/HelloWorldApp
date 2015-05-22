ab6ca01244d8c4095fa4f48223d5ecdbb1b0b687
Stefan Knoll <stefan@knollfamily.de>
2015-05-22 12:17:35 +0200

    Removed some comments only for Git testing purpose

--- src/helloworldapp/HelloWorldApp.java
+++ src/helloworldapp/HelloWorldApp.java
@@ -11,9 +11,6 @@ package helloworldapp;
  */
 public class HelloWorldApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
